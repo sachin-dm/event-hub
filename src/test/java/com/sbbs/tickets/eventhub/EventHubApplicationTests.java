7d56f3fe95afb7cf39e8ba726c574090ba39743b
Sachin Mhetre <66195001+sachin-dm@users.noreply.github.com>
2024-12-23 23:23:28 +0530

    Initial commit

---
+++ src/test/java/com/sbbs/tickets/eventhub/EventHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbbs.tickets.eventhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
