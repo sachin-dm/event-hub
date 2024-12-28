8b11c426976ee6aa720f69ff026f848bd862e556
Sachin Mhetre <66195001+sachin-dm@users.noreply.github.com>
2024-12-29 01:21:35 +0530

    Docker changes and API changes

--- src/main/java/com/sbbs/tickets/eventhub/repository/VenueRepository.java
+++ src/main/java/com/sbbs/tickets/eventhub/repository/VenueRepository.java
@@ -1,6 +1,5 @@
 package com.sbbs.tickets.eventhub.repository;
 
-import com.sbbs.tickets.eventhub.model.Event;
 import com.sbbs.tickets.eventhub.model.Venue;
 import org.springframework.data.jpa.repository.JpaRepository;
 
