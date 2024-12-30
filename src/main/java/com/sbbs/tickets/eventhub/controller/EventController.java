aaf2a37527723243a72c3c24b40d373b49ae70a0
Sachin Mhetre <66195001+sachin-dm@users.noreply.github.com>
2024-12-30 11:17:04 +0530

    Updated README.md

--- src/main/java/com/sbbs/tickets/eventhub/controller/EventController.java
+++ src/main/java/com/sbbs/tickets/eventhub/controller/EventController.java
@@ -3,19 +3,12 @@ package com.sbbs.tickets.eventhub.controller;
 import com.sbbs.tickets.eventhub.model.Event;
 import com.sbbs.tickets.eventhub.service.EventService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
@@ -29,29 +22,22 @@ public class EventController {
     @GetMapping("/{eventId}")
     public ResponseEntity<Event> getEvent(@PathVariable String eventId) {
         Event event = eventService.getEvent(eventId);
-        if (event != null) {
-            return ResponseEntity.ok(event);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+        return event != null ? ResponseEntity.ok(event) : ResponseEntity.notFound().build();
     }
 
     @GetMapping("/list")
     public Page<Event> listEvents(@RequestParam(defaultValue = "0") int page,
-                                     @RequestParam(defaultValue = "50") int size,
-                                     @RequestParam(defaultValue = "id") String sortBy,
-                                     @RequestParam(defaultValue = "true") boolean ascending) {
+                                  @RequestParam(defaultValue = "50") int size,
+                                  @RequestParam(defaultValue = "id") String sortBy,
+                                  @RequestParam(defaultValue = "true") boolean ascending) {
         Sort sort = ascending ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending();
         Pageable pageable = PageRequest.of(page, size, sort);
-
         return eventService.listEvents(pageable);
     }
 
     @PostMapping("/save")
     public ResponseEntity<Void> createEvent(@RequestBody Event event) {
         eventService.saveEvent(event);
-        return ResponseEntity
-                .created(URI.create("/events/" + event.getId()))
-                .build();
+        return ResponseEntity.created(URI.create("/events/" + event.getId())).build();
     }
 }
--- src/main/java/com/sbbs/tickets/eventhub/controller/UserController.java
+++ src/main/java/com/sbbs/tickets/eventhub/controller/UserController.java
@@ -8,13 +8,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
@@ -28,11 +22,7 @@ public class UserController {
     @GetMapping("/{userId}")
     public ResponseEntity<EventUser> getUser(@PathVariable String userId) {
         EventUser eventUser = userService.getUser(userId);
-        if (eventUser != null) {
-            return ResponseEntity.ok(eventUser);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+        return eventUser != null ? ResponseEntity.ok(eventUser) : ResponseEntity.notFound().build();
     }
 
     @GetMapping("/list")
@@ -42,15 +32,12 @@ public class UserController {
                                      @RequestParam(defaultValue = "true") boolean ascending) {
         Sort sort = ascending ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending();
         Pageable pageable = PageRequest.of(page, size, sort);
-
         return userService.listUsers(pageable);
     }
 
     @PostMapping("/save")
     public ResponseEntity<Void> createUser(@RequestBody EventUser eventUser) {
         userService.saveUser(eventUser);
-        return ResponseEntity
-                .created(URI.create("/users/" + eventUser.getId()))
-                .build();
+        return ResponseEntity.created(URI.create("/users/" + eventUser.getId())).build();
     }
 }
